9dc4899ddd0f475f4a6e0b21d84c96625bb5957d
Josu Uribe Sainz <juribe@gfi.es>
2018-12-20 16:42:12 +0100

    Change names

--- nCubed.EFCore.Test/EntityFramework/Queryable.cs
+++ nCubed.EFCore.Test/EntityFramework/Queryable.cs
@@ -8,6 +8,7 @@ using System.Text;
 using Xunit;
 using System.Linq;
 using nCubed.EFCore.Test.Repositories.Fakes;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace nCubed.EFCore.Test.EntityFramework
 {
@@ -15,12 +16,17 @@ namespace nCubed.EFCore.Test.EntityFramework
     {
         private readonly CustomerRepository customerRepository = null;
 
-        public Queryable()
-        {
-            ContactInformation contactInformation = new ContactInformation() { Phone = "phone", Email = "demo@demo.es" };
-            Customer customer = new Customer() { Name = "Customer", ContactInformation = contactInformation };
+        //public TestBetween()
+        //{
+        //    ContactInformation contactInformation1 = new ContactInformation() { Phone = "phone1", Email = "demo1@demo.es" };
+        //    Customer customer1 = new Customer() { Name = "Customer1", ContactInformation = contactInformation1 };
+        //    ContactInformation contactInformation2 = new ContactInformation() { Phone = "phone2", Email = "demo2@demo.es" };
+        //    Customer customer2 = new Customer() { Name = "Customer2", ContactInformation = contactInformation2 };
 
-            customerRepository = new CustomerRepository(new ProjectsContext(dbContextOptions));
-        }
+        //    customerRepository = new CustomerRepository(new ProjectsContext(dbContextOptions));
+        //    customerRepository.Add(customer1).Add(customer2);
+
+        //    customerRepository.Set().Between<Customer, int>()
+        //}
     }
 }
