57bc9aaa08c308534bd49dc318c2cedeec7deb4c
Josu Uribe Sainz <juribe@gfi.es>
2019-01-03 09:52:08 +0100

    Added version in .nuspec

---
+++ nCubedConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using nCubed.EFCore.Repositories;
+
+namespace nCubedConsole
+{
+    public class Customer { }
+
+    public class Repository : nCubed.EFCore.Repositories.IRepository<Customer>
+    {
+        public IUnitOfWork UnitOfWork { get; }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Repository r = new Repository();
+        }
+    }
+}
