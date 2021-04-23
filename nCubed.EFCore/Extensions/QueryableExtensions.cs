417d23e190fff3247e2ea88a57c3021733c06bcc
Josu Uribe Sainz <juribe@plainconcepts.com>
2021-04-23 10:34:57 +0200

    Added distinct

--- nCubed.EFCore/Extensions/QueryableExtensions.cs
+++ nCubed.EFCore/Extensions/QueryableExtensions.cs
@@ -1,14 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
-using Microsoft.EntityFrameworkCore.Query;
-using Microsoft.EntityFrameworkCore.Query.Internal;
-using Microsoft.EntityFrameworkCore.Storage;
-using Remotion.Linq.Parsing.Structure;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
-using System.Text;
 
 namespace nCubed.EFCore.Extensions
 {
@@ -20,15 +13,15 @@ namespace nCubed.EFCore.Extensions
         /// <typeparam name="TSource">Entity to use.</typeparam>
         /// <typeparam name="TKey">Between property to use.</typeparam>
         /// <param name="source">Query with original data.</param>
-        /// <param name="property">Property to be used in between command.</param>
+        /// <param name="keySelector">Property to be used in between command.</param>
         /// <param name="low">Minimun value.</param>
         /// <param name="high">Maximun value.</param>
         /// <returns></returns>
-        public static IQueryable<TSource> Between<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> property, TKey low, TKey high) where TKey : IComparable<TKey>
+        public static IQueryable<TSource> Between<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, TKey low, TKey high) where TKey : IComparable<TKey>
         {
             var sourceParameter = Expression.Parameter(typeof(TSource));
-            var body = property.Body;
-            var parameter = property.Parameters[0];
+            var body = keySelector.Body;
+            var parameter = keySelector.Parameters[0];
             var compareMethod = typeof(TKey).GetMethod("CompareTo", new Type[] { typeof(TKey) });
             var zero = Expression.Constant(0, typeof(int));
             var upper = Expression.LessThanOrEqual(Expression.Call(body, compareMethod, Expression.Constant(high)), zero);
@@ -36,6 +29,33 @@ namespace nCubed.EFCore.Extensions
             var andExpression = Expression.AndAlso(upper, lower);
             var whereCallExpression = Expression.Call(typeof(Queryable), "Where", new Type[] { source.ElementType }, source.Expression, Expression.Lambda<Func<TSource, Boolean>>(andExpression, new ParameterExpression[] { parameter }));
             return source.Provider.CreateQuery<TSource>(whereCallExpression);
+        }
+        /// <summary>
+        /// Returns distinct elements based on property
+        /// </summary>
+        /// <typeparam name="TSource">Entity to use.</typeparam>
+        /// <typeparam name="TKey">Distinct property to use.</typeparam>
+        /// <param name="source">Query with original data.</param>
+        /// <param name="keySelector"></param>
+        /// <returns>Filtered list</returns>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var list = source.ToList();
+
+            for (int i = 0; i < list.Count(); i++)
+            {
+                var from = list[i];
+
+                for (int j = i + 1; j < list.Count(); j++)
+                {
+                    var to = list[j];
+                    if (keySelector(from).Equals(keySelector(to)))
+                    {
+                        list.Remove(to);
+                    }
+                }
+                yield return from;
+            }
         }
     }
 }
